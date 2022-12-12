fdf6a724d4116d6b6c820472516e41793471188b
mark-todd <markpeter.todd@hotmail.co.uk>
2022-12-12 10:21:37 +0000

    Fixed example

--- examples/test.py
+++ examples/test.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-from fast_binomial import FastBinomial
+from fast_binomial import SFC64, Generator
 
 test = np.array([[0, 3, 0], [0, 1, 3]], dtype=int)
-
-gen = FastBinomial(0.5, 1)
-print(gen.generate(test))
+gen = Generator(SFC64(), 0.5)
+results = gen.binomial(test)
+print(results)
