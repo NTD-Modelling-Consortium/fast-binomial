e46ad1b1a928e6001d69283d8d86d03f141c18c9
Marcin K <ma@rcin.me>
2023-01-31 10:58:48 +0100

    Fixing the build process -- moved build.py into new setup.py (#7)
    * Fixing the build process -- moved build.py into new setup.py
    
    * Regenerated lockfile - reformatted setup
    
    ---------
    
    Co-authored-by: mark-todd <markpeter.todd@hotmail.co.uk>

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from pathlib import Path
 
 import requests
 from pybind11.setup_helpers import Pybind11Extension
+from setuptools import setup
 
 VENDOR_DIR = Path(__file__).resolve().parent / "vendor"
 
@@ -78,3 +79,34 @@ def build(setup_kwargs):
             "zip_safe": False,
         }
     )
+
+
+packages = ["fast_binomial"]
+
+package_data = {"": ["*"]}
+
+install_requires = [
+    "numpy>=1.23.5,<2.0.0",
+    "pybind11>=2.10.1,<3.0.0",
+    "requests>=2.28.1,<3.0.0",
+]
+
+setup_kwargs = {
+    "name": "fast-binomial",
+    "version": "0.1.0",
+    "description": "",
+    "long_description": "None",
+    "author": "mark-todd",
+    "author_email": "markpeter.todd@hotmail.co.uk",
+    "maintainer": "None",
+    "maintainer_email": "None",
+    "url": "None",
+    "packages": packages,
+    "package_data": package_data,
+    "install_requires": install_requires,
+    "python_requires": ">=3.10,<4.0",
+}
+
+build(setup_kwargs)
+
+setup(**setup_kwargs)
